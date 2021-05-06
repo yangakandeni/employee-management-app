c8d6d377e9e6a2cd8a73319cbd5f88a917b5f2b2
yangakandeni <kandeni.yanga>
2021-05-06 11:21:49 +0200

    add new staff to dict

--- employee.py
+++ employee.py
@@ -1,20 +1,26 @@
 class Employee:
     # stores all the created employees
-    employees = dict()
+    employees = {}
 
     def __init__(self, name, employee_id, department, job_title):
         self.name = name
         self.department = department
         self.employee_id = employee_id
         self.job_title = job_title
-
-        # saves an employee instance to the employees dict
-        Employee.employees.setdefault(self.employee_id, {
+        # create staff dict to store employee details
+        self.staff = {
             'Name': self.name,
             'ID': self.employee_id,
             'Department': self.department,
             'Position': self.job_title
-        })
+        }
+
+        # assign employee details to staff dict
+        Employee.employees.setdefault(self.employee_id, self.staff)
+
+    @classmethod
+    def display_employees(cls):
+        return cls.employees
 
     def __str__(self):
         return f"\nEmployee Name: {self.name}\nID Number: {self.employee_id}\nDepartment: {self.department}\nJob Title: {self.job_title}"
--- tests/test_employee.py
+++ tests/test_employee.py
@@ -8,7 +8,7 @@ class EmployeeTestCase(unittest.TestCase):
         self.employee_1 = Employee(name="Mike", employee_id="12345",
                         department="Engineering", job_title="Developer")
 
-        self.employee_2 = Employee(name="Yanga", employee_id="12345",
+        self.employee_2 = Employee(name="Yanga", employee_id="54321",
                                    department="Engineering", job_title="Developer")
         return super().setUp()
 
@@ -21,7 +21,7 @@ class EmployeeTestCase(unittest.TestCase):
         self.assertEqual(mike.job_title, "Developer")
         self.assertEqual(mike.__str__(
         ), f"\nEmployee Name: {mike.name}\nID Number: {mike.employee_id}\nDepartment: {mike.department}\nJob Title: {mike.job_title}")
-        self.assertCountEqual(Employee.employees, 2)
+        self.assertEqual(len(Employee.employees), 2)
     
     def test_can_store_employees(self):
         #
